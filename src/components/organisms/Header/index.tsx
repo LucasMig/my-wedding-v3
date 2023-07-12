6a1936b3a7d97d63317a92c45d57fe535272b0cf
LucasMig <lucasmigliori@gmail.com>
2023-07-11 22:36:32 -0300

    feat: Finished mobile navigation layout

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -2,13 +2,14 @@ import { navItems } from '@/components/organisms/Header/data';
 
 import './styles.scss';
 import useScreenWidth from '@/hooks/useScreenWidth';
-import NavLink from '@/components/atoms/NavLink';
 import { useState } from 'react';
+import NavList from '@/components/molecules/NavList';
+import { scrollToAnchor } from '@/utils/helpers';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(true);
   const { isMobile } = useScreenWidth();
-  const mapItemID = navItems.find((item) => item.href === '#map')?.id;
+  const mapItem = navItems.find((item) => item.href === '#map');
 
   const handleMenuOpen = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -17,11 +18,8 @@ const Header = () => {
   return (
     <header className="header">
       {isMobile && (
-        <div className={`${isMenuOpen ? 'menu--open' : 'menu--closed'}`} />
-      )}
-
-      {isMobile ? (
-        <>
+        <div>
+          <div className={`${isMenuOpen ? 'menu--open' : 'menu--closed'}`} />
           <nav
             className={`navigation__container--mobile ${
               isMenuOpen ? 'open' : ''
@@ -34,22 +32,42 @@ const Header = () => {
               onClick={handleMenuOpen}
             />
             <ul className="navigation__list">
-              {navItems
-                .filter((item) => item.id !== mapItemID)
-                .map((item) => {
-                  const { id, href, label } = item;
-
-                  return <NavLink key={id} label={label} href={href} />;
-                })}
+              <NavList
+                navItems={navItems.filter((item) => item.id !== mapItem?.id)}
+              />
             </ul>
           </nav>
+        </div>
+      )}
+      <div className="header__navbar">
+        {isMobile ? (
           <img
-            src="/images/logo-square-no-bg.webp"
-            alt="pri-e-lucas-logo"
-            className="navigation__logo"
+            src="/icons/hamburger.webp"
+            alt="menu-icon"
+            className="navigation__icon"
+            onClick={handleMenuOpen}
           />
-        </>
-      ) : null}
+        ) : (
+          <NavList navItems={navItems.slice(0, navItems.length / 2)} />
+        )}
+        <img
+          src="/images/logo-square-no-bg.webp"
+          alt="pri-e-lucas-logo"
+          className="navigation__logo"
+        />
+        {isMobile ? (
+          <img
+            src="/icons/location.webp"
+            alt="location-icon"
+            className="navigation__icon"
+            onClick={() => {
+              scrollToAnchor(mapItem?.href ?? '#');
+            }}
+          />
+        ) : (
+          <NavList navItems={navItems.slice(navItems.length / 2)} />
+        )}
+      </div>
     </header>
   );
 };
