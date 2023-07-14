229d1d0c7a48498dbb672a496b948b86ce9288a2
LucasMig <lucasmigliori@gmail.com>
2023-07-14 16:02:27 -0300

    feat: finished banner mobile layout

--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -1,15 +1,18 @@
 import AttendanceForm from '@/components/organisms/AttendanceForm';
+import Banner from '@/components/organisms/Banner';
 import Header from '@/components/organisms/Header';
 import Minimap from '@/components/organisms/Minimap';
+import Timer from '@/components/organisms/Timer';
 // import Timer from '@/components/organisms/Timer';
 
 const HomePage = () => {
   return (
     <>
       <Header />
+      <Banner imgPath="/images/banner.webp" />
+      <Timer />
       <AttendanceForm />
       <Minimap />
-      {/* <Timer /> */}
     </>
   );
 };
