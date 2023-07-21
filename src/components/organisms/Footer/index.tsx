92eb689c294e6c5f9b71f22a5800779a33c1d0ab
LucasMig <lucasmigliori@gmail.com>
2023-07-21 12:57:57 -0300

    feat: finished footer mobile version

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -1,7 +1,27 @@
+import Typography from '@/components/atoms/Typography';
 import './styles.scss';
+import { CURRENT_YEAR } from '@/shared/constants';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <img src="/images/logo-round.webp" alt="logo" className="footer__logo" />
+      <div className="footer__content">
+        <Typography styleClass="footer__text">Feito com 💙</Typography>
+        <Typography styleClass="footer__text">
+          por{' '}
+          <a
+            href="https://www.linkedin.com/in/lucas-migliori/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Lucas Migliori
+          </a>
+        </Typography>
+        <Typography styleClass="footer__text">© {CURRENT_YEAR}</Typography>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
