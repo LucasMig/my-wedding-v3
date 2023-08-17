04420cdd01be271caec9a0d01991aa83d5a2153e
LucasMig <lucasmigliori@gmail.com>
2023-08-17 09:56:20 -0300

    fix:

--- src/hooks/useGeolocation.ts
+++ src/hooks/useGeolocation.ts
@@ -5,35 +5,38 @@ const useGeolocation = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isError, setIsError] = useState<string | null>(null);
 
-  const getGeolocation = async () => {
-    return await new Promise<GeolocationPosition>((resolve, reject) => {
-      setIsLoading(true);
+  const getGeolocation = (
+    successCallback: (position: GeolocationPosition) => void,
+    errorCallback: (error: GeolocationPositionError) => void,
+  ) => {
+    setIsLoading(true);
 
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            resolve(position);
-          },
-          (error) => {
-            reject(error.message);
-          },
-        );
-      } else {
-        reject(new Error('Geolocation is not supported by this browser.'));
-      }
-    });
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setIsLoading(false);
+          successCallback(position);
+        },
+        (error) => {
+          setIsLoading(false);
+          errorCallback(error);
+        },
+      );
+    } else {
+      setIsLoading(false);
+      setIsError('Geolocation is not supported by this browser.');
+    }
   };
 
   useEffect(() => {
-    getGeolocation()
-      .then((position) => {
+    getGeolocation(
+      (position) => {
         setLocation(position);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        setIsError(error);
-        setIsLoading(false);
-      });
+      },
+      (error) => {
+        setIsError(error.message);
+      },
+    );
   }, []);
 
   return { location, isLoading, isError };
